c3191164e1bc9cfac43627b700626999c192d240
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-13 13:44:55 -0800

    Wrote time signatures material.

---
+++ armilla/materials/time_signatures/definition.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+time_signatures = indicatortools.TimeSignatureInventory([
+    (1, 4),
+    (1, 8),
+    (2, 4),
+    (2, 8),
+    (3, 4),
+    (3, 8),
+    (4, 4),
+    (4, 8),
+    (5, 4),
+    (5, 8),
+    (6, 8),
+    ])
---
+++ armilla/materials/time_signatures/output.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import indicatortools
+
+
+time_signatures = indicatortools.TimeSignatureInventory(
+    [
+        indicatortools.TimeSignature((1, 4)),
+        indicatortools.TimeSignature((1, 8)),
+        indicatortools.TimeSignature((2, 4)),
+        indicatortools.TimeSignature((2, 8)),
+        indicatortools.TimeSignature((3, 4)),
+        indicatortools.TimeSignature((3, 8)),
+        indicatortools.TimeSignature((4, 4)),
+        indicatortools.TimeSignature((4, 8)),
+        indicatortools.TimeSignature((5, 4)),
+        indicatortools.TimeSignature((5, 8)),
+        indicatortools.TimeSignature((6, 8)),
+        ]
+    )
