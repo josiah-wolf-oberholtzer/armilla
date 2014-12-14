ee075592bd06352662b66ed4cfd79ea7cfccfbfd
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-13 19:10:16 -0800

    Added "bow rhythm maker a" material.

---
+++ armilla/materials/bow_rhythm_maker_a/output.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import durationtools
+from abjad.tools import rhythmmakertools
+
+
+bow_rhythm_maker_a = rhythmmakertools.EvenDivisionRhythmMaker(
+    denominators=(8, 8, 4, 16, 4, 16, 8, 8, 4),
+    extra_counts_per_division=(0, 0, 1, 0, 2, 1, 0, 0, 3, 2, 1),
+    duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+        decrease_durations_monotonically=True,
+        forbidden_written_duration=durationtools.Duration(1, 2),
+        permit_meter_rewriting=False,
+        ),
+    )
