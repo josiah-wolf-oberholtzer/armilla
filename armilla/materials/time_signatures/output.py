d041df9084789527ec01a2ea1b0ab970a95c10e7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-16 00:06:31 -0800

    Simplified time signature inventory.

--- armilla/materials/time_signatures/output.py
+++ armilla/materials/time_signatures/output.py
@@ -7,9 +7,7 @@ time_signatures = indicatortools.TimeSignatureInventory(
         indicatortools.TimeSignature((2, 4)),
         indicatortools.TimeSignature((3, 4)),
         indicatortools.TimeSignature((3, 8)),
-        indicatortools.TimeSignature((4, 4)),
         indicatortools.TimeSignature((4, 8)),
-        indicatortools.TimeSignature((5, 4)),
         indicatortools.TimeSignature((5, 8)),
         indicatortools.TimeSignature((6, 8)),
         ]
