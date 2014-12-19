a08741843c61ad2da0c513e65eb66172dd4ef70b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-18 23:36:46 -0500

    Updated bow rhythm maker.

--- armilla/materials/bow_rhythm_maker_a/definition.py
+++ armilla/materials/bow_rhythm_maker_a/definition.py
@@ -4,10 +4,10 @@ from abjad import *
 
 bow_rhythm_maker_a = rhythmmakertools.EvenDivisionRhythmMaker(
     denominators=(8, 8, 4, 16, 4, 16, 8, 8, 4),
-    extra_counts_per_division=(0, 0, 1, 0, 2, 1, 0, 0, 3, 2, 1),
+    extra_counts_per_division=(0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1),
     duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
         decrease_durations_monotonically=True,
-        forbidden_written_duration=durationtools.Duration(1, 2),
+        forbidden_written_duration=durationtools.Duration(1, 4),
         permit_meter_rewriting=False,
         ),
     )
