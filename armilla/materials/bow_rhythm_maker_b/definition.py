b70f3404c5772155ca4b4f79332f24c6c0b6bec6
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-24 00:14:40 -0500

    Added bow rhythm maker b material.

---
+++ armilla/materials/bow_rhythm_maker_b/definition.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import consort
+
+
+
+bow_rhythm_maker_b = consort.CompositeRhythmMaker(
+    last=rhythmmakertools.IncisedRhythmMaker(
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            prefix_counts=[0],
+            suffix_talea=[1],
+            suffix_counts=[1],
+            talea_denominator=16,
+            ),
+        ),
+    rest=rhythmmakertools.EvenDivisionRhythmMaker(
+        denominators=(8, 8, 4, 16, 4, 16, 8, 8, 4),
+        extra_counts_per_division=(0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1),
+        duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+            decrease_durations_monotonically=True,
+            forbidden_written_duration=durationtools.Duration(1, 4),
+            permit_meter_rewriting=False,
+            ),
+        ),
+    )
