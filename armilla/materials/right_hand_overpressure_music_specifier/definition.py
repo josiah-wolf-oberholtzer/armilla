d18a69fac7d0fb7b870a24ef2060d85784058ef9
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-30 18:43:20 -0800

    Improved right-hand overpressure material.

--- armilla/materials/right_hand_overpressure_music_specifier/definition.py
+++ armilla/materials/right_hand_overpressure_music_specifier/definition.py
@@ -1,5 +1,81 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+import consort
+from abjad.tools import durationtools
+from abjad.tools import indicatortools
+from abjad.tools import rhythmmakertools
+from abjad.tools import scoretools
+from abjad.tools import selectortools
+from abjad.tools import spannertools
 
 
-right_hand_overpressure_music_specifier = None
+right_hand_overpressure_music_specifier = consort.MusicSpecifier(
+    attachment_handler=consort.AttachmentHandler(
+        bow_contact_points=consort.AttachmentExpression(
+            attachments=(
+                indicatortools.BowContactPoint(0),
+                indicatortools.BowContactPoint((1, 8)),
+                indicatortools.BowContactPoint((1, 4)),
+                indicatortools.BowContactPoint((3, 8)),
+                indicatortools.BowContactPoint((1, 2)),
+                indicatortools.BowContactPoint((5, 8)),
+                indicatortools.BowContactPoint((3, 4)),
+                indicatortools.BowContactPoint((7, 8)),
+                indicatortools.BowContactPoint(1),
+                indicatortools.BowContactPoint(0),
+                indicatortools.BowContactPoint((1, 8)),
+                indicatortools.BowContactPoint(0),
+                indicatortools.BowContactPoint((1, 8)),
+                indicatortools.BowContactPoint((1, 4)),
+                indicatortools.BowContactPoint((1, 8)),
+                indicatortools.BowContactPoint((3, 8)),
+                indicatortools.BowContactPoint((1, 4)),
+                indicatortools.BowContactPoint((3, 8)),
+                indicatortools.BowContactPoint((1, 2)),
+                indicatortools.BowContactPoint((5, 8)),
+                indicatortools.BowContactPoint((3, 4)),
+                indicatortools.BowContactPoint((7, 8)),
+                indicatortools.BowContactPoint(1),
+                ),
+            selector=selectortools.Selector().by_leaves().flatten(),
+            ),
+        bow_contact_spanner=spannertools.BowContactSpanner(),
+        dynamic_expressions=consort.DynamicExpression(
+            dynamic_tokens='p ppp p ppp p ppp f',
+            ),
+        string_contact_points=consort.AttachmentExpression(
+            attachments=(
+                indicatortools.StringContactPoint('ordinario'),
+                None,
+                None,
+                indicatortools.StringContactPoint('sul tasto'),
+                None,
+                None,
+                indicatortools.StringContactPoint('molto sul tasto'),
+                None,
+                ),
+            scope=scoretools.Voice,
+            selector=selectortools.Selector().with_callback(
+                consort.PhrasedSelectorCallback())
+            ),
+        string_contact_spanner=consort.StringContactSpanner(),
+        ),
+    rhythm_maker=consort.CompositeRhythmMaker(
+        last=rhythmmakertools.IncisedRhythmMaker(
+            incise_specifier=rhythmmakertools.InciseSpecifier(
+                prefix_counts=[0],
+                suffix_talea=[1],
+                suffix_counts=[1],
+                talea_denominator=16,
+                ),
+            ),
+        default=rhythmmakertools.EvenDivisionRhythmMaker(
+            denominators=(4,),
+            extra_counts_per_division=(0, 0, 0, 1, 0, 0, 1, 0, 1),
+            duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+                decrease_durations_monotonically=True,
+                forbidden_written_duration=durationtools.Duration(1, 4),
+                permit_meter_rewriting=False,
+                ),
+            ),
+        ),
+    )
--- armilla/materials/right_hand_overpressure_music_specifier/output.py
+++ armilla/materials/right_hand_overpressure_music_specifier/output.py
@@ -1,4 +1,173 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import datastructuretools
+from abjad.tools import durationtools
+from abjad.tools import indicatortools
+from abjad.tools import rhythmmakertools
+from abjad.tools import scoretools
+from abjad.tools import selectortools
+from abjad.tools import spannertools
+import consort
 
 
-right_hand_overpressure_music_specifier = None
+right_hand_overpressure_music_specifier = consort.tools.MusicSpecifier(
+    attachment_handler=consort.tools.AttachmentHandler(
+        bow_contact_points=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(0, 1),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 4),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(3, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 2),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(5, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(3, 4),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(7, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 1),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(0, 1),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(0, 1),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 4),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(3, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 4),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(3, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 2),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(5, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(3, 4),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(7, 8),
+                        ),
+                    indicatortools.BowContactPoint(
+                        contact_point=durationtools.Multiplier(1, 1),
+                        ),
+                    ]
+                ),
+            selector=selectortools.Selector(
+                callbacks=(
+                    selectortools.PrototypeSelectorCallback(
+                        prototype=scoretools.Leaf,
+                        ),
+                    selectortools.FlattenSelectorCallback(
+                        depth=-1,
+                        ),
+                    ),
+                ),
+            ),
+        bow_contact_spanner=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    spannertools.BowContactSpanner(),
+                    ]
+                ),
+            ),
+        dynamic_expressions=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    consort.tools.DynamicExpression(
+                        dynamic_tokens=datastructuretools.CyclicTuple(
+                            ['p', 'ppp', 'p', 'ppp', 'p', 'ppp', 'f']
+                            ),
+                        transitions=datastructuretools.CyclicTuple(
+                            [None]
+                            ),
+                        ),
+                    ]
+                ),
+            ),
+        string_contact_points=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    indicatortools.StringContactPoint(
+                        contact_point='ordinario',
+                        ),
+                    None,
+                    None,
+                    indicatortools.StringContactPoint(
+                        contact_point='sul tasto',
+                        ),
+                    None,
+                    None,
+                    indicatortools.StringContactPoint(
+                        contact_point='molto sul tasto',
+                        ),
+                    None,
+                    ]
+                ),
+            selector=selectortools.Selector(
+                callbacks=(
+                    consort.tools.PhrasedSelectorCallback(),
+                    ),
+                ),
+            scope=scoretools.Voice,
+            ),
+        string_contact_spanner=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    consort.tools.StringContactSpanner(),
+                    ]
+                ),
+            ),
+        ),
+    rhythm_maker=consort.tools.CompositeRhythmMaker(
+        last=rhythmmakertools.IncisedRhythmMaker(
+            incise_specifier=rhythmmakertools.InciseSpecifier(
+                prefix_counts=(0,),
+                suffix_talea=(1,),
+                suffix_counts=(1,),
+                talea_denominator=16,
+                ),
+            ),
+        default=rhythmmakertools.EvenDivisionRhythmMaker(
+            denominators=(4,),
+            extra_counts_per_division=(0, 0, 0, 1, 0, 0, 1, 0, 1),
+            duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+                decrease_durations_monotonically=True,
+                forbidden_written_duration=durationtools.Duration(1, 4),
+                permit_meter_rewriting=False,
+                ),
+            ),
+        ),
+    )
